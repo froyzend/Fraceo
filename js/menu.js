d8aadd8555dc52c0aa4bdb7105732c99c8530004
Froyzend <froyzend1993@gmail.com>
2024-12-07 15:08:17 +0100

    fini

--- js/menu.js
+++ js/menu.js
@@ -1,22 +1,28 @@
-/*const openModalButton = document.querySelector(".open-menu-button");
-const modal = document.querySelector(".menu-header");
-const closeModalButton = document.querySelector(".close-button");
+document.addEventListener("DOMContentLoaded", () => {
+  const openModalButton = document.querySelector(".open-menu-button");
+  const modal = document.querySelector(".menu-header");
+  const closeModalButton = document.querySelector(".close-button");
+  const menuButton = document.querySelector(".menu-button");
 
-openModalButton.addEventListener("click", () => {
-  modal.style.display = "flex";
-});
+  if (openModalButton && modal && closeModalButton) {
+    openModalButton.addEventListener("click", () => {
+      modal.style.display = "flex";
+    });
 
-closeModalButton.addEventListener("click", () => {
-  modal.style.display = "none";
-});
+    closeModalButton.addEventListener("click", () => {
+      modal.style.display = "none";
+    });
 
-modal.addEventListener("click", (e) => {
-  if (e.target === modal) {
-    modal.style.display = "none";
+    modal.addEventListener("click", (e) => {
+      if (e.target === modal) {
+        modal.style.display = "none";
+      }
+    });
+  }
+
+  if (menuButton) {
+    menuButton.addEventListener("click", () => {
+      menuButton.classList.toggle("open");
+    });
   }
 });
-const menuButton = document.querySelector(".menu-button");
-menuButton.addEventListener("click", () => {
-  menuButton.classList.toggle("open");
-});
-*/
--- js/scroll.js
+++ js/scroll.js
@@ -1,19 +1,24 @@
 let scrollToTopBtn = document.getElementById("scrollToTopBtn");
+if (scrollToTopBtn) {
+  window.onscroll = function () {
+    if (
+      document.body.scrollTop > 100 ||
+      document.documentElement.scrollTop > 100
+    ) {
+      scrollToTopBtn.style.display = "block";
+    } else {
+      scrollToTopBtn.style.display = "none";
+    }
+  };
 
-window.onscroll = function () {
-  if (
-    document.body.scrollTop > 100 ||
-    document.documentElement.scrollTop > 100
-  ) {
-    scrollToTopBtn.style.display = "block";
-  } else {
-    scrollToTopBtn.style.display = "none";
-  }
-};
-
-scrollToTopBtn.addEventListener("click", function () {
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth",
+  scrollToTopBtn.addEventListener("click", function () {
+    if ("scrollBehavior" in document.documentElement.style) {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    } else {
+      window.scrollTo(0, 0);
+    }
   });
-});
+}
